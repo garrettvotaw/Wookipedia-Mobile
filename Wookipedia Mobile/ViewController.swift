f462d230e6b12e24e2de16065953497bcad790d9
Garrett Votaw <garrettvotaw@Garretts-MacBook-Pro.local>
2017-12-01 11:48:52 -0600

    Initial Commit

---
+++ Wookipedia Mobile/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Wookipedia Mobile
+//
+//  Created by Garrett Votaw on 12/1/17.
+//  Copyright © 2017 Garrett Votaw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Wookipedia MobileTests/Wookipedia_MobileTests.swift
@@ -0,0 +1,36 @@
+//
+//  Wookipedia_MobileTests.swift
+//  Wookipedia MobileTests
+//
+//  Created by Garrett Votaw on 12/1/17.
+//  Copyright © 2017 Garrett Votaw. All rights reserved.
+//
+
+import XCTest
+@testable import Wookipedia_Mobile
+
+class Wookipedia_MobileTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
